92291086766f1549a44e24aacec7357e6e2011bf
Brice Lof <b.lof@iciformation.fr>
2017-01-10 11:29:24 +0100

    commit avant pull pour récupérer travaux fait durant mon absence à la réunion

--- src/Application/PlateformeBundle/Resources/public/js/main.js
+++ src/Application/PlateformeBundle/Resources/public/js/main.js
@@ -21,13 +21,13 @@ $(function(){
                 
                 $.ajax({
                     type: 'get',
-                    url: "http://localhost/Enteo/web/detail_statut/"+statutId,
+                    url: Routing.generate("application_plateforme_detail_statut", { idStatut: statutId }),
                     beforeSend: function(){
                         console.log('ça charge')
                         //$(".detailStatut").append(
                     }
                 }).done(function(data) {
-                    console.log(data.details)
+                    console.log(data)
                     $(".detailStatut").html("");
                     for(var i = 0; i < data.details.length; i++)
                     {
