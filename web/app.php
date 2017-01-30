ca4f6231cddfca9700e129f0f10a05dcfc464e20
Brice Lof <b.lof@iciformation.fr>
2017-01-30 10:30:50 +0100

    Chargement des css et js en mode prod

--- web/app.php
+++ web/app.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-Debug::enable();
+//Debug::enable();
 //include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', true);
