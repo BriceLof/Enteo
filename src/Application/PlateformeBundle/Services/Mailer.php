74a19209748351657097937be01e974fe051c2e9
Brice <b.lof@iciformation.fr>
2017-01-30 14:54:31 +0100

    probleme d'affichage document

--- src/Application/PlateformeBundle/Services/Mailer.php
+++ src/Application/PlateformeBundle/Services/Mailer.php
@@ -56,9 +56,11 @@ class Mailer
             // beneficiaire ayant fini le RV1
             $nextStep = $newsRepo->findOneBy(array('beneficiaire' => $beneficiaireID, 'statut' => '4'));
             var_dump("beneficiaire ID : ".$beneficiaireID." | RV2 : ".count($nextStep));
+			var_dump($nextStep);
             // envoi email en fonction du temps passé entre la RV1 et la date du jour 
             if(is_null($nextStep))
             {
+				var_dump($rv->getBeneficiaire()->getConsultant());exit;
                 $subject = "Post RV1";
                 $template = "@Apb/Alert/Mail/postRv1.html.twig";
                 $body = $this->templating->render($template, array(
