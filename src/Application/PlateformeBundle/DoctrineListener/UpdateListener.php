0b85aeb32d90474b6c14b4f72309dd6962b293c9
Brice <b.lof@iciformation.fr>
2017-10-04 14:25:00 +0200

    Notification fonctionnant pour l'update d'informations sur la fiche bénéficiaire. Next => notif lors de l'ajout

--- src/Application/PlateformeBundle/DoctrineListener/UpdateListener.php
+++ src/Application/PlateformeBundle/DoctrineListener/UpdateListener.php
@@ -11,44 +11,68 @@ namespace Application\PlateformeBundle\DoctrineListener;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Application\PlateformeBundle\Entity\Beneficiaire;
-use Application\PlateformeBundle\Entity\Accompagnement;
 
 class UpdateListener extends \Twig_Extension
 {
     private $mailer;
     private $container;
+    private $em;
+    private $currentUser;
+    private $entity;
 
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
+    /*
+     * Envoi un mail dès la mise à jour d'une entité lié au bénéficiaire, si ce n'est pas le consultant qui s'en est charger.
+     */
     public function postUpdate(LifecycleEventArgs $args)
     {
-        $currentUser = $this->container->get('security.token_storage')->getToken()->getUser();
+        $this->currentUser = $this->container->get('security.token_storage')->getToken()->getUser();
         $this->mailer = $this->container->get('application_plateforme.mail');
-        $entity = $args->getObject();
+        $this->em = $this->container->get('doctrine.orm.entity_manager');
+        // L'entité touché lors de l'update
+        $this->entity = $args->getObject();
 
-        $subject = "Notification : mise à jour de votre bénéficiaire";
-        // Si l'utilisateur courant n'est pas le consultant du beneficiaire, on envoi un mail de notification
-        if($entity instanceof Beneficiaire  && ($currentUser != $entity->getConsultant()))
-        {
-            $consultant = $entity->getConsultant();
-            if(!is_null($consultant)) {
-                $beneficiaire = $entity;
-                $message = "
-                Bonjour " . ucfirst($consultant->getCivilite()) . " " . ucfirst($consultant->getPrenom()) . " " . strtoupper($consultant->getNom()) . ",<br><br>
-                
-                La fiche de votre bénéficiaire <b></b><a href='//appli.entheor.com/web/beneficiaire/show/" . $beneficiaire->getId() . "'>" . ucfirst($beneficiaire->getCiviliteConso()) . " " . ucfirst($beneficiaire->getPrenomConso()) . " " . strtoupper($beneficiaire->getNomConso()) . "</a></b> a été mis à jour.  
-                ";
-                $this->mailer->sendNewNotification($consultant->getEmail(), $subject, $message);
-            }
+        // Obtenir le nom de l'entité utilisé (=> le nom de sa classe)
+        $entityNameOriginal = $this->em->getMetadataFactory()->getMetadataFor(get_class($this->entity))->getName();
+        $entityNameExplode = explode('\\', $entityNameOriginal);
+        $entityName = end($entityNameExplode);
+        // Liste des entités touchant la page bénéficiaire dont on veut avoir les notifications
+        $listEntity = array(
+            "Accompagnement"    => "Accompagnement",
+            "Nouvelle"          => "News",
+            "Document"          => "Espace Documentaire",
+            "Historique"        => "Historique bénéficiaire",
+            "News"              => "Statut bénéficiaire",
+        );
+        // Vérification, si l'entité touché lors de l'update fait partie de notre tableau d'entités
+        if(array_key_exists($entityName, $listEntity) !== false){
+            $section = $listEntity[$entityName];
+            $this->notificationByEntity($this->entity, $entityName, $section);
+        }
+    }
+
+    public function notificationByEntity($entity, $theEntityName, $section)
+    {
+        if($theEntityName == "Accompagnement") {
+            $beneficiaireRepository = $this->em->getRepository(Beneficiaire::class);
+            $beneficiaire = $beneficiaireRepository->findOneByAccompagnement($entity);
+        }else{
+            $beneficiaire = $entity->getBeneficiaire();
         }
 
-        if($entity instanceof Accompagnement)
-        {
-
+        $consultant = $beneficiaire->getConsultant();
+        if (!is_null($consultant) && ($this->currentUser != $consultant)) {
+            $subject = "Notification : mise à jour de votre bénéficiaire";
+            $message = "
+                    Bonjour " . ucfirst($consultant->getCivilite()) . " " . ucfirst($consultant->getPrenom()) . " " . strtoupper($consultant->getNom()) . ",<br><br>                
+                    La fiche de votre bénéficiaire <b></b><a href='//appli.entheor.com/web/beneficiaire/show/" . $beneficiaire->getId() . "'>" . ucfirst($beneficiaire->getCiviliteConso()) . " " . ucfirst($beneficiaire->getPrenomConso()) . " " . strtoupper($beneficiaire->getNomConso()) . "</a></b> a été mis à jour.<br>  
+                    Voir dans la partie '".$section."'.  
+                    ";
+            $this->mailer->sendNewNotification($consultant->getEmail(), $subject, $message);
         }
-
     }
 }
