2ee65fdf45c4c88db11a9ae0855d2768d3a29058
Brice <b.lof@iciformation.fr>
2017-10-18 10:13:50 +0200

    fix bug par fidy dans generation pdf

--- src/Application/UsersBundle/Form/UsersType.php
+++ src/Application/UsersBundle/Form/UsersType.php
@@ -25,132 +25,129 @@ class UsersType extends AbstractType
     {
         $this->manager = $manager;
     }*/
-    
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-                ->add('roles', ChoiceType::class, array(
-                    'choices' => array(
-                        //'Basic'             =>  "ROLE_USER",
-                        'Gestionnaire'      =>  "ROLE_GESTION",
-                        'Consultant'        =>  "ROLE_CONSULTANT",
-                        'Commercial'        =>  "ROLE_COMMERCIAL",
-                        'Administrateur'    =>  "ROLE_ADMIN",
-                    ),
-                    //'mapped' => false,
-                    'label' => "Type Utilisateur *",
-                    'required' => true,
-                    'expanded' => true,
-                    'multiple' => true,
-                    //'data' => array("ROLE_USER"),
-                    'choice_attr' => function($val, $key, $index) {
-                       // adds a class like attending_yes, attending_no, etc
-                       return ['class' => 'role_user'];
-                    },
-                    
-                ))
-                ->add('calendrierid', TextType::class, array(
-                    'label' => 'Calendrier ID',
-                    'required' => false,
-                ))
-                ->add('calendrieruri', TextareaType::class, array(
-                    'label' => 'Calendrier URI',
-                    'required' => false,
-                ))
-                ->add('civilite', ChoiceType::class, array(
-                    'choices' => array(
-                        'M.'                =>  "m.",
-                        'Mme'               =>  "mme",
-                    ),
-                    'label' => "Civilité",
-                    'multiple' => false,
-                    'expanded' => true,
-                    'required' => true,
-                    
-                    
-                ))
-                ->add('nom', null, array(
-                    'required'  => true
-                ))
-                ->add('prenom', null, array(
-                    'required'  => true
-                ))
-                ->add('tel1', TextType::class, array(
-                    'label'     => 'Tél 1 *', 
-                    'required'  => true
-                ))
-                ->add('tel2', TextType::class, array(
-                    'label'     => 'Tél 2', 
-                    'required'  => false
-                ))
-                ->add('email2', EmailType::class, array(
-                    'label'     => 'Email 2', 
-                    'required'  => false
-                ))
-                ->add('distanciel', ChoiceType::class, array(
-                    'choices' => array('Présentiel'    =>  'presentiel', 'Distanciel'    =>  'distanciel' ),
-                    'expanded' => true,
-                    'multiple' => true,
-                    'required' => true,
-                    /*'label_attr' => array('class' => 'format_label', 'style' => 'display:none;'),*/
-                    'choice_attr' => function($val, $key, $index) {
-                       return ['class' => 'format_input'];
-                    },
-                ))
-                
-                // Me sert juste à activer l'ajax pour la recherche de ville correspondant
-                ->add('departement', TextType::class, array(
-                    'mapped' => false, 
-                    'label' => "Département *", 
-                    'required' => true, 
-                    'attr' => array("maxlength" => 2, "class" => "departementInputForAjax")
-                ))
-                ->add('codePostalHidden', HiddenType::class, array("mapped" => false))
-                ->add('typeUserHidden', HiddenType::class, array("mapped" => false))
-                ->add('idVilleHidden', HiddenType::class, array("mapped" => false))
-				;
-				
-               /* if(isset($options['data']))
-                {
-                    //var_dump($options['data']);
-                    $builder->add('ville', EntityType::class, array(
-                        'class' => 'ApplicationPlateformeBundle:Ville',
-                        'label' => 'Ville *',
-                        'query_builder' => function (EntityRepository $er) use ($options) {
-                            return $er->createQueryBuilder('v')
-                                    ->where('v.id = :id')
-                                    ->setParameter('id',$options['data']->getVille()->getId() )
-                                ->orderBy('v.nom', 'ASC')
-                                ->setMaxResults( 1 );
-                        },
-                        'choice_label' => 'nom',
-                    ));
-                }
-                else{*/
-                    $builder->add('ville', EntityType::class, array(
-                        'class' => 'ApplicationPlateformeBundle:Ville',
-                        'label' => 'Ville *',
-                        'query_builder' => function (EntityRepository $er) {
-                            return $er->createQueryBuilder('v')
-                                ->orderBy('v.nom', 'ASC')
-                                ->setMaxResults( 1 );
-                        },
-                        'choice_label' => 'nom',
-                    ));
-               // }
-                $builder->add('adresse', TextType::class, array(
-                    'label' => 'Adresse ', 
-                    'required' => false, 
-                ))
-                ;
-                //if(isset($options['data'])) var_dump($options['data']->getVille());
+            ->add('roles', ChoiceType::class, array(
+                'choices' => array(
+                    //'Basic'             =>  "ROLE_USER",
+                    'Gestionnaire' => "ROLE_GESTION",
+                    'Consultant' => "ROLE_CONSULTANT",
+                    'Commercial' => "ROLE_COMMERCIAL",
+                    'Administrateur' => "ROLE_ADMIN",
+                ),
+                //'mapped' => false,
+                'label' => "Type Utilisateur *",
+                'required' => true,
+                'expanded' => true,
+                'multiple' => true,
+                //'data' => array("ROLE_USER"),
+                'choice_attr' => function ($val, $key, $index) {
+                    // adds a class like attending_yes, attending_no, etc
+                    return ['class' => 'role_user'];
+                },
+
+            ))
+            ->add('calendrierid', TextType::class, array(
+                'label' => 'Calendrier ID',
+                'required' => false,
+            ))
+            ->add('calendrieruri', TextareaType::class, array(
+                'label' => 'Calendrier URI',
+                'required' => false,
+            ))
+            ->add('civilite', ChoiceType::class, array(
+                'choices' => array(
+                    'M.' => "m.",
+                    'Mme' => "mme",
+                ),
+                'label' => "Civilité",
+                'multiple' => false,
+                'expanded' => true,
+                'required' => true,
+
+
+            ))
+            ->add('nom', null, array(
+                'required' => true
+            ))
+            ->add('prenom', null, array(
+                'required' => true
+            ))
+            ->add('tel1', TextType::class, array(
+                'label' => 'Tél 1 *',
+                'required' => true
+            ))
+            ->add('tel2', TextType::class, array(
+                'label' => 'Tél 2',
+                'required' => false
+            ))
+            ->add('email2', EmailType::class, array(
+                'label' => 'Email 2',
+                'required' => false
+            ))
+            ->add('distanciel', ChoiceType::class, array(
+                'choices' => array('Présentiel' => 'presentiel', 'Distanciel' => 'distanciel'),
+                'expanded' => true,
+                'multiple' => true,
+                'required' => true,
+                /*'label_attr' => array('class' => 'format_label', 'style' => 'display:none;'),*/
+                'choice_attr' => function ($val, $key, $index) {
+                    return ['class' => 'format_input'];
+                },
+            ))
+            // Me sert juste à activer l'ajax pour la recherche de ville correspondant
+            ->add('departement', TextType::class, array(
+                'mapped' => false,
+                'label' => "Département *",
+                'required' => true,
+                'attr' => array("maxlength" => 2, "class" => "departementInputForAjax")
+            ))
+            ->add('codePostalHidden', HiddenType::class, array("mapped" => false))
+            ->add('typeUserHidden', HiddenType::class, array("mapped" => false))
+            ->add('idVilleHidden', HiddenType::class, array("mapped" => false));
+
+        /* if(isset($options['data']))
+         {
+             //var_dump($options['data']);
+             $builder->add('ville', EntityType::class, array(
+                 'class' => 'ApplicationPlateformeBundle:Ville',
+                 'label' => 'Ville *',
+                 'query_builder' => function (EntityRepository $er) use ($options) {
+                     return $er->createQueryBuilder('v')
+                             ->where('v.id = :id')
+                             ->setParameter('id',$options['data']->getVille()->getId() )
+                         ->orderBy('v.nom', 'ASC')
+                         ->setMaxResults( 1 );
+                 },
+                 'choice_label' => 'nom',
+             ));
+         }
+         else{*/
+        $builder->add('ville', EntityType::class, array(
+            'class' => 'ApplicationPlateformeBundle:Ville',
+            'label' => 'Ville *',
+            'query_builder' => function (EntityRepository $er) {
+                return $er->createQueryBuilder('v')
+                    ->orderBy('v.nom', 'ASC')
+                    ->setMaxResults(1);
+            },
+            'choice_label' => 'nom',
+        ));
+        // }
+        $builder->add('adresse', TextType::class, array(
+            'label' => 'Adresse ',
+            'required' => false,
+        ));
+        //if(isset($options['data'])) var_dump($options['data']->getVille());
         /*$builder->get('ville')
             ->addModelTransformer(new VilleToNumberTransformer($this->manager));*/
     }
-    
+
     public function getParent()
     {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType';
@@ -158,9 +155,8 @@ class UsersType extends AbstractType
         // Or for Symfony < 2.8
         // return 'fos_user_registration';
     }
-    
-   
-    
+
+
     /**
      * {@inheritdoc}
      */
