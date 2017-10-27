13821abf765a40791b971c493f369e93c9527d74
Brice <b.lof@iciformation.fr>
2017-10-27 11:46:17 +0200

    reglage du cron a rdv +1 et rdv +2

--- src/Application/PlateformeBundle/Command/MailRvCommand.php
+++ src/Application/PlateformeBundle/Command/MailRvCommand.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace Application\PlateformeBundle\Command;
+
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\ORM\Mapping as ORM;
-class MailRvCommand extends ContainerAwareCommand{
+
+class MailRvCommand extends ContainerAwareCommand
+{
     protected function configure()
     {
         $this
@@ -14,20 +18,18 @@ class MailRvCommand extends ContainerAwareCommand{
                       1) CRON chaque jour à 21h00 et si statut bénéficiaire passe de R1 ou R2 à faire à R1 ou R2 positif
                       2) Si à h+24 de l'heure du Rdv la fiche bénéficiaire N'EST PAS MAJ => +++ Relance email (1b) au Consultant 
                       3) Si à h+48 de l'heure du Rdv la fiche bénéficiaire N'EST PAS MAJ => Relance email (1c) au Consultant ")
-            ->addOption('yell', null, InputOption::VALUE_NONE, 'Si définie, la tâche criera en majuscules')
-        ;
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'Si définie, la tâche criera en majuscules');
     }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
         $consultants = $em->getRepository('ApplicationUsersBundle:Users')->findAll();
-        foreach ( $consultants as $consultant) {
-
-            $tab2 = null;
-            $tab3 = null;
+        foreach ($consultants as $consultant) {
+            $tab2 = array();
+            $tab3 = array();
 
             $beneficiaires = $consultant->getBeneficiaire();
-            $now = new \DateTime();
             foreach ($beneficiaires as $beneficiaire) {
                 $news = $beneficiaire->getNews();
                 //CRON chaque jour à 21h00 et si statut bénéficiaire passe de R1 ou R2 à faire à R1 ou R2 positif +++ => Envoyer email
@@ -39,7 +41,7 @@ class MailRvCommand extends ContainerAwareCommand{
                     $slugNextToNewsStatus = $nextToNews->getStatut()->getSlug();
                     //si le statut est : RV1 réalisé ou RV2 réalisé
 //                    if ($slugLastNewsStatus == "rv1-realise" || $slugLastNewsStatus == "rv2-realise") {
-                    if ($lastNews->getDetailStatut()->getDetail() == "RV1 Positif" || $lastNews->getDetailStatut()->getDetail() == "RV2 Positif" ){
+                    if ($lastNews->getDetailStatut()->getDetail() == "RV1 Positif" || $lastNews->getDetailStatut()->getDetail() == "RV2 Positif") {
                         //si l'avant dernier statut est : RV1 à faire ou RV2 à faire
                         if ($slugNextToNewsStatus == "rv1-a-faire" || $slugNextToNewsStatus == "rv2-a-faire") {
                             $dateLastNews = $lastNews->getDateHeure();
@@ -58,67 +60,43 @@ class MailRvCommand extends ContainerAwareCommand{
                     }
                 }
 
+                $historiques = $beneficiaire->getHistorique();
 
-                //Si à h+24 de l'heure du Rdv la fiche bénéficiaire N'EST PAS MAJ => +++ Relance email (1b) au Consultant
-                $majBene = $beneficiaire->getUpdatedAt();
-                $historique = $beneficiaire->getHistorique();
-                $lastHistorique = $historique[count($historique) - 1];
-                //si le dernier historique existe?
-                if (!is_null($lastHistorique)) {
+//              boucler sur toutes les historiques
+                foreach ($historiques as $historique) {
 
-                    $now = new \DateTime();
+                    $rv = substr($historique->getSummary(), 0, 2);
 
-                    $rv = substr($lastHistorique->getSummary(), 0, 2);
-                    //si le dernier historique, son summary commence par "RV" car les autres historique n'a pas de summary
-                    if ($rv == "RV") {
-                        $lastHistorique->getDateDebut()->modify('+1 day');
-                        //si la date de ce dernier historique date de mois de 24h au moment du lancement de la cron journalier
-                        //et que la fiche bénéficiaire n'est pas mis a jour
-                        //envoi un mail au consultant
-                        if ($lastHistorique->getDateDebut() > $now->modify('-1 day')) {
-                            $now->modify('+1 day');
-                            if ($lastHistorique->getDateDebut() <= $now) {
-                                //si la date du dernier historique n'est pas anterieure a la date du dernier mis a jour
-                                if ($lastHistorique->getDateDebut() > $beneficiaire->getUpdatedAt()->modify('+1 day')) {
+                    //si l'historique n'est pas archivé ou annulé ou modifié
+                    if ($historique->getEventarchive() == 'off' && $historique->getCanceled() == 0) {
+                        //si la date de cet historique date de mois de 24h au moment du lancement de la cron journalier
+                        if ($historique->getDateDebut() > (new \DateTime('now'))->modify('-1 day') && $historique->getDateDebut() < (new \DateTime('now'))) {
+
+                            if ($rv == "RV" && $beneficiaire->getUpdatedAt() < (new \DateTime('now'))->modify('-1 day')) {
+                                if (!in_array($beneficiaire, $tab2)){
                                     $tab2[] = $beneficiaire;
-                                    $beneficiaire->getUpdatedAt()->modify('-1 day');
-                                } else {
-                                    $beneficiaire->getUpdatedAt()->modify('-1 day');
                                 }
                             }
-                        } else {
-                            $now->modify('+1 day');
                         }
-                        $lastHistorique->getDateDebut()->modify('+1 day');
-                        //si la date de ce dernier historique date de mois de 48h au moment du lancement de la cron journalier
-                        //et que la fiche bénéficiaire n'est pas mis a jour
-                        //envoi un mail au consultant
-                        if ($lastHistorique->getDateDebut() > $now->modify('-1 day')) {
 
-                            $now->modify('+1 day');
-
-                            if ($lastHistorique->getDateDebut() <= $now) {
-                                //si la date du dernier historique n'est pas anterieure a la date du dernier mis a jour
-                                if ($lastHistorique->getDateDebut() > $beneficiaire->getUpdatedAt()->modify('+2 day')) {
+                        //si la date de cet historique date de mois de 48h au moment du lancement de la cron journalier
+                        if ($historique->getDateDebut() > (new \DateTime('now'))->modify('-2 day') && $historique->getDateDebut() < (new \DateTime('now'))->modify('-1 day')) {
+                            if ($rv == "RV" && $beneficiaire->getUpdatedAt() < (new \DateTime('now'))->modify('-2 day')) {
+                                if (!in_array($beneficiaire, $tab3)) {
                                     $tab3[] = $beneficiaire;
-                                    $beneficiaire->getUpdatedAt()->modify('-2 day');
-                                } else {
-                                    $beneficiaire->getUpdatedAt()->modify('-2 day');
                                 }
                             }
-                        } else {
-                            $now->modify('+1 day');
                         }
-                        //mis a jour de la date du dernier historique
-                        $lastHistorique->getDateDebut()->modify('-2 day');
                     }
                 }
+
             }
 
-            if ($tab2 != null){
+            if ($tab2 != null) {
                 $this->getContainer()->get('application_plateforme.statut.cron.rv')->firstMailRvFicheNonMaj($consultant, $tab2);
             }
-            if ($tab3 != null){
+
+            if ($tab3 != null) {
                 $this->getContainer()->get('application_plateforme.statut.cron.rv')->secondMailRvFicheNonMaj($consultant, $tab3);
             }
         }
