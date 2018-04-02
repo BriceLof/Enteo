68b99ef009dcaa6b09c0745f3cdd8a5f349fb1c0
Brice <b.lof@iciformation.fr>
2018-04-02 15:23:41 +0200

    nothing

--- src/Application/PlateformeBundle/Services/Mailer.php
+++ src/Application/PlateformeBundle/Services/Mailer.php
@@ -156,7 +156,7 @@ class Mailer
             'reference' => 'Notification'
         ));
         $bcc = array("support.informatique@entheor.com" => "Support", "f.azoulay@entheor.com" => "Franck Azoulay");
-        
+
         $this->sendMessage($this->from,$to,null,$cc = null, $bcc ,$subject,$body);
     }
 
