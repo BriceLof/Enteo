a9098b7a343f2e20a8b496a4a3a17609ac5c4b77
Brice <b.lof@iciformation.fr>
2019-05-12 10:30:26 +0200

    version dev

--- src/Application/UsersBundle/Controller/UsersController.php
+++ src/Application/UsersBundle/Controller/UsersController.php
@@ -19,30 +19,29 @@ use Symfony\Component\HttpFoundation\Request;
 
 class UsersController extends Controller
 {
-    
+
     public function indexAction($typeUser = null, Request $request)
     {
         $etat = $request->query->get("actif");
         $em = $this->getDoctrine()->getManager();
-        if(is_null($typeUser)){
-            if($etat == 'true')
+        if (is_null($typeUser)) {
+            if ($etat == 'true')
                 $users = $em->getRepository('ApplicationUsersBundle:Users')->findBy(array('enabled' => 1), array("nom" => "ASC"));
-            elseif($etat == 'false')
+            elseif ($etat == 'false')
                 $users = $em->getRepository('ApplicationUsersBundle:Users')->findBy(array('enabled' => 0), array("nom" => "ASC"));
             else
-                $users = $em->getRepository('ApplicationUsersBundle:Users')->findBy(array(), array("nom" => "ASC"));  
-        }
-        else{
-            if($etat == 'true')
+                $users = $em->getRepository('ApplicationUsersBundle:Users')->findBy(array(), array("nom" => "ASC"));
+        } else {
+            if ($etat == 'true')
                 $users = $em->getRepository('ApplicationUsersBundle:Users')->findByTypeUser($typeUser, 1);
-            elseif($etat == 'false')
+            elseif ($etat == 'false')
                 $users = $em->getRepository('ApplicationUsersBundle:Users')->findByTypeUser($typeUser, 0);
             else
                 $users = $em->getRepository('ApplicationUsersBundle:Users')->findByTypeUser($typeUser);
         }
-            
+
         return $this->render('ApplicationUsersBundle:Users:index.html.twig', array(
-            'users' => $users,  
+            'users' => $users,
         ));
     }
 
@@ -64,8 +63,8 @@ class UsersController extends Controller
             'form' => $form->createView(),
         ));
     }
-    
-    public function showAction(Request $request,Users $user)
+
+    public function showAction(Request $request, Users $user)
     {
 
         $em = $em = $this->getDoctrine()->getManager();
@@ -101,45 +100,38 @@ class UsersController extends Controller
     public function editAction(Request $request, Users $user)
     {
         // supression du salt dans le password avant de le mettre dans le champs du form 
-        $user->setPassword(substr($user->getPassword(), 0,-45));
-        
-        //$user->setRoles(array("ROLE_CONSULTANT"));
-        //var_dump($user->getRoles());
-        
+        $user->setPassword(substr($user->getPassword(), 0, -45));
+
         $editForm = $this->get("form.factory")->create(UsersType::class, $user);
-        if(!is_null($user->getVille()))
-        {
-            $editForm->get('departement')->setData(substr($user->getVille()->getCp(),0,2));
+
+        if (!is_null($user->getVille())) {
+            $editForm->get('departement')->setData(substr($user->getVille()->getCp(), 0, 2));
             $editForm->get('codePostalHidden')->setData($user->getVille()->getCp());
-            $editForm->get('idVilleHidden')->setData($user->getVille()->getId()); 
+            $editForm->get('idVilleHidden')->setData($user->getVille()->getId());
         }
-        
+
 
         $typeUser = $this->container->get('application_users.getTypeUser');
         $editForm->get('typeUserHidden')->setData($typeUser->typeUser($this->getUser()));
-        
-        if ($request->isMethod('POST') && $editForm->handleRequest($request)->isValid()) {
+
+        $editForm->handleRequest($request);
+
+        if ($request->isMethod('POST') && $editForm->isSubmitted()) {
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
             $request->getSession()->getFlashBag()->add('info', 'Utilisateur modifié avec succès');
-            return $this->redirectToRoute('user_show', array('id' => $user->getId()));  
+            return $this->redirectToRoute('user_show', array('id' => $user->getId()));
         }
-        /*
-        $breadcrumbs = $this->get("white_october_breadcrumbs");
-        // Simple example
-        $breadcrumbs->addItem("Home", $this->get("router")->generate("user_type"));
-        $breadcrumbs->addItem("Utilisateur", $this->get("router")->generate("user_type"));
-        $breadcrumbs->addItem("Modification");  
-        // Example with parameter injected into translation "user.profile"
-    */
+
         return $this->render('ApplicationUsersBundle:Users:edit.html.twig', array(
             'user' => $user,
             'form' => $editForm->createView()
         ));
     }
-    
+
     // Désactiver un utilisateur (pas de suppression car bénéficiaire est lié à un consultant) 
     public function turnOffAction(Request $request, Users $user)
     {
@@ -150,12 +142,11 @@ class UsersController extends Controller
             //var_dump($user->getId());exit;
             $em = $this->getDoctrine()->getManager();
             //$em->remove($user);
-            if($user->isEnabled() == true){
+            if ($user->isEnabled() == true) {
                 $user->setEnabled(0);
                 $em->flush($user);
                 $request->getSession()->getFlashBag()->add('info', 'Utilisateur désactivé avec succès');
-            }
-            else{
+            } else {
                 $user->setEnabled(1);
                 $em->flush($user);
                 $request->getSession()->getFlashBag()->add('info', 'Utilisateur activé avec succès');
@@ -172,11 +163,10 @@ class UsersController extends Controller
 
     private function createDeleteForm(Users $user)
     {
-        if($user->isEnabled() == true){
+        if ($user->isEnabled() == true) {
             $btn = "Désactiver";
             $bootstrapClass = "btn-danger";
-        }
-        else{
+        } else {
             $btn = "Activer";
             $bootstrapClass = "btn-success";
         }
@@ -189,12 +179,11 @@ class UsersController extends Controller
             ))
             ->add("delete", \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, array(
                 'label' => $btn,
-                'attr' => array('class' => 'btn '.$bootstrapClass)
+                'attr' => array('class' => 'btn ' . $bootstrapClass)
             ))
-            ->getForm()
-            ;
+            ->getForm();
     }
-    
+
     public function myAccountAction(Request $request)
     {
         $myself = $this->getUser();
@@ -206,8 +195,8 @@ class UsersController extends Controller
         if ($request->isMethod('POST')) {
             $file = $myself->getImage();
 
-            if(!is_null($file)){
-                $fileName = $this->get('app.file_uploader')->uploadAvatar($file, 'uploads/profile/'.$myself->getId(), $lastPicture);
+            if (!is_null($file)) {
+                $fileName = $this->get('app.file_uploader')->uploadAvatar($file, 'uploads/profile/' . $myself->getId(), $lastPicture);
                 $myself->setImage($fileName);
             }
             $em = $this->getDoctrine()->getManager();
@@ -222,57 +211,45 @@ class UsersController extends Controller
 
         return $this->render('ApplicationUsersBundle:Users:account.html.twig', array(
             'form' => $form->createView(),
-            'user' => $myself, 
+            'user' => $myself,
             'typeUser' => $typeUser->typeUser($myself),
             'nbDossiersEnCours' => $nbDossiersEnCours
         ));
     }
-    
+
     public function myAccountEditAction(Request $request)
     {
         $user = $this->getUser();
         // supression du salt dans le password avant de le mettre dans le champs du form 
-        $user->setPassword(substr($user->getPassword(), 0,-45));
+        $user->setPassword(substr($user->getPassword(), 0, -45));
         //$user->setRoles(array("ROLE_CONSULTANT"));
-        
+
         $editForm = $this->get("form.factory")->create(UsersType::class, $user);
-        $editForm->get('departement')->setData(substr($user->getVille()->getCp(),0,2));
+        $editForm->get('departement')->setData(substr($user->getVille()->getCp(), 0, 2));
         $editForm->get('codePostalHidden')->setData($user->getVille()->getCp());
         $editForm->get('idVilleHidden')->setData($user->getVille()->getId());
 
         $typeUser = $this->container->get('application_users.getTypeUser');
         $editForm->get('typeUserHidden')->setData($typeUser->typeUser($this->getUser()));
-        
+
         if ($request->isMethod('POST') && $editForm->handleRequest($request)->isValid()) {
-//        	$avatarFile = $user->getAvatar();
-		 	//$fileUploader = $this->container->get('app.file_uploader');
-			//$fileUploader->upload($avatarFile, "uploads/avatars");
-			
-			// Generate a unique name for the file before saving it
-//            $fileName = md5(uniqid()).'.'.$avatarFile->guessExtension();
-//
-//            // Move the file to the directory where brochures are stored
-//            $avatarFile->move(
-//                $this->getParameter('avatars_directory'),
-//                $fileName
-//            );
-//			$user->setAvatar($fileName);
-			
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
             $request->getSession()->getFlashBag()->add('info', 'Votre compte a été modifié avec succès');
-            return $this->redirectToRoute('my_account');  
+            return $this->redirectToRoute('my_account');
         }
-       
+
         return $this->render('ApplicationUsersBundle:Users:edit.html.twig', array(
             'user' => $user,
             'form' => $editForm->createView()
         ));
     }
 
-    public function ajaxGetUserAction(Request $request, $id){
+    public function ajaxGetUserAction(Request $request, $id)
+    {
         if ($request->isXmlHttpRequest()) {
             $em = $this->getDoctrine()->getManager();
             $user = $em->getRepository('ApplicationUsersBundle:Users')->find($id);
@@ -284,12 +261,13 @@ class UsersController extends Controller
             );
             $response = new JsonResponse();
             return $response->setData(array('data' => $consultant));
-        }else{
+        } else {
             throw new \Exception('erreur');
         }
     }
 
-    public function getAllCommercialAction(){
+    public function getAllCommercialAction()
+    {
         $em = $this->getDoctrine()->getManager();
         $commercials = $em->getRepository('ApplicationUsersBundle:Users')->findByTypeUser('ROLE_COMMERCIAL', true);
 
@@ -298,7 +276,8 @@ class UsersController extends Controller
         ));
     }
 
-    public function editDeclarationActiviteAction(Request $request){
+    public function editDeclarationActiviteAction(Request $request)
+    {
         $consultant = $this->getUser();
 
         $form = $this->createFormBuilder($consultant)
